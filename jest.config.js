c5c234ddab8c511e1af8690b82f6c1a693d8c664
Nickolas Kenyeres <nickolas@knicklabs.com>
2018-11-27 15:01:03 -0500

    Add setup script for Jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ const jestConfig = require('@helpscout/zero/jest')
 
 module.exports = Object.assign(jestConfig, {
   coveragePathIgnorePatterns: ['src/index.js'],
+  setupTestFrameworkScriptFile: '<rootDir>jest.setup.js',
   testURL: 'http://localhost/',
 })
