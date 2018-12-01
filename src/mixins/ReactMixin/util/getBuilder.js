8867f1bcb531ca9e1f46af966a2f93a2e564c21d
Nickolas Kenyeres <nickolas@knicklabs.com>
2018-12-01 00:15:26 -0500

    Fix typo

--- src/mixins/ReactMixin/util/getBuilder.js
+++ src/mixins/ReactMixin/util/getBuilder.js
@@ -1,5 +1,5 @@
 /**
- * Get the builder. This me be a React component that can be used as is or it
+ * Get the builder. This may be a React component that can be used as is or it
  * may be props used as instructions to create a React component. It could even
  * be a function which can be invoked to return a React component or props.
  *
@@ -8,7 +8,7 @@
  * @returns {ReactNode|Object|function|undefined}
  */
 const getBuilder = (components, selector) => {
-  return components[selector];
-};
+  return components[selector]
+}
 
-export default getBuilder;
+export default getBuilder
