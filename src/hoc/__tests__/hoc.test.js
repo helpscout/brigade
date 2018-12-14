cae2a3e7034aa4eba98b4cd79b34a19a4b356b16
Jon Q <hello@jonquach.com>
2018-12-14 11:33:11 -0500

    Fix Docz + Babel + Jest (with latest Zero)

---
+++ src/hoc/__tests__/hoc.test.js
@@ -0,0 +1,9 @@
+import {ReactMounter, ReactView} from '../index'
+
+test('ReactMounter should be exported', () => {
+  expect(ReactMounter).toBeTruthy()
+})
+
+test('ReactView should be exported', () => {
+  expect(ReactView).toBeTruthy()
+})
