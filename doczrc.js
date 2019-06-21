abf9396402b3213e181fe3bafc8298c9be4a1214
Nickolas Kenyeres <nickolas@knicklabs.com>
2019-06-21 11:32:29 -0400

    Update Docz config

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,8 @@
 export default {
   title: 'Brigade',
-  description: 'Backbone-controlled React Component',
+  description: 'Backbone-controlled React Components',
+  indexHtml: 'public/index.html',
+  public: './public',
   themeConfig: {
     colors: {
       primary: '#22A1F0',
